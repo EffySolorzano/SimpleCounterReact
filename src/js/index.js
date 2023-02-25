8ec296fc20702acd81cd3c8f311ba8174ed773b0
EffySolorzano <solorzano.steph@gmail.com>
2023-02-25 03:46:10 +0000

    simplecounterfinished

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,57 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
+import SecondsCounter from "./component/secondscounter.jsx";
 import Home from "./component/home.jsx";
 
+
+
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let tiempoSegundos = 0;
+let decenaSeconds = 0;
+let centenaSeconds =0;
+
+
+setInterval(() => {
+    tiempoSegundos = tiempoSegundos + 1
+    /*tiempoSegundos = tiempoSegundos + 1
+    if(tiempoSegundos>9){
+        tiempoSegundos=0
+        decenaSeconds++
+        if(decenaSeconds>5){
+            decenaSeconds=0
+            centenaSeconds++
+        }
+    } 
+     if(tiempoSegundos%10==0 && tiempoSegundos!=0){
+        tiempoSegundos=0
+        decenaSeconds++
+    }
+    if(decenaSeconds%6==0 && decenaSeconds!=0){
+        decenaSeconds=0
+        centenaSeconds++
+    }*/
+
+//Careful when working with switch, don't forget to add default. 
+switch(true){
+    case tiempoSegundos>9:
+        tiempoSegundos=0
+        decenaSeconds++
+        break;
+    case decenaSeconds>5:
+        decenaSeconds=0
+        centenaSeconds++
+        break;
+    default:
+        break
+};
+
+
+  ReactDOM.render(
+    <><Home />
+    <SecondsCounter centenaSeconds={centenaSeconds} decenaSeconds={decenaSeconds} seconds={tiempoSegundos} />
+    </>,
+    document.querySelector("#app")
+  );
+}, 1000);
